ef15d511ed199b532dcff471f13d9a0f1e5a7d4f
eleprince01 <eleprince01@SEC-C210-F05.csip.in>
2025-03-25 14:17:29 +0100

    classe operation

---
+++ src/main/java/fr/insa/leprince/projets2/Operation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.leprince.projets2;
+
+/**
+ *
+ * @author eleprince01
+ */
+public class Operation {
+    private String operation;
+    private String dOperation;
+    private float dureeOperation;
+    private String refEquipement;
+}
