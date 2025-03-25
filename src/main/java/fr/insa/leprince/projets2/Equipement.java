3663cd0593aa668f5c05b413b0bf72193cc28b9f
clema <clema@172.29.105.218>
2025-03-25 13:55:47 +0100

    Ajout classe equipement

---
+++ src/main/java/fr/insa/leprince/projets2/Equipement.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.leprince.projets2;
+
+/**
+ *
+ * @author clema
+ */
+public class Equipement {
+     private String refEquipement;
+    private String dEquipement;
+
+    public String getRefEquipement() {
+        return refEquipement;
+    }
+
+    public String getdEquipement() {
+        return dEquipement;
+    }
+
+    public void setRefEquipement(String refEquipement) {
+        this.refEquipement = refEquipement;
+    }
+
+    public void setdEquipement(String dEquipement) {
+        this.dEquipement = dEquipement;
+    }
+
+    public Equipement(String refEquipement, String dEquipement) {
+        this.refEquipement = refEquipement;
+        this.dEquipement = dEquipement;
+    }
+}
