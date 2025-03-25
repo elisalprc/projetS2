c897ea21b841515fa83c6bb5f4538665f6a3cc2d
eleprince01 <eleprince01@SEC-C210-F05.csip.in>
2025-03-25 13:58:49 +0100

    classe produit

---
+++ src/main/java/fr/insa/leprince/projets2/Produit.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.leprince.projets2;
+
+/**
+ *
+ * @author eleprince01
+ */
+public class Produit {
+    private String codeProduit;
+    private String dProduit;
+
+    //getter et setter 
+    public String getCodeProduit() {
+        return codeProduit;
+    }
+
+    public void setCodeProduit(String codeProduit) {
+        this.codeProduit = codeProduit;
+    }
+
+    public String getdProduit() {
+        return dProduit;
+    }
+
+    public void setdProduit(String dProduit) {
+        this.dProduit = dProduit;
+    }
+    
+    //fin getter et setter 
+    
+    
+}
