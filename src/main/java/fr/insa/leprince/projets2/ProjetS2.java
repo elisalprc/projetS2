45724ae529e4a1d2c7969a9f2eee12affc8ad13f
eleprince01 <eleprince01@SEC-C210-F05.csip.in>
2025-03-25 13:31:34 +0100

    test : vide

---
+++ src/main/java/fr/insa/leprince/projets2/ProjetS2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.insa.leprince.projets2;
+
+/**
+ *
+ * @author eleprince01
+ */
+public class ProjetS2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
