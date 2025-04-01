3c671ace367e8376f9513feba420ca76126a9698
chach <chach@172.17.33.38>
2025-04-01 15:12:52 +0200

    ici patate de l'espace : entrée des première méthodes de Gamme

--- src/main/java/fr/insa/leprince/projets2/Gamme.java
+++ src/main/java/fr/insa/leprince/projets2/Gamme.java
@@ -5,18 +5,26 @@
 package fr.insa.leprince.projets2;
 
 import java.util.ArrayList;
+import java.Atelirt.listGamme;
 
 /**
  *
  * @author eleprince01
  */
 public class Gamme {
+//Atributs:
     private String refGamme;
     private ArrayList<Operation> listeOperations;
     private ArrayList<Equipement> listeEquipement;
     
-    public Gamme(){}
+//Constructeur:
+    public Gamme(String refGamme, ArrayList<Operation> listeOperations, ArrayList<Equipement> listeEquipement){
+        this.listeEquipement=listeEquipement;
+        this.listeOperations=listeOperations;
+        this.refGamme=refGamme;
+    }
 
+//Getters et setters:
     public String getRefGamme() {
         return refGamme;
     }
@@ -41,8 +49,27 @@ public class Gamme {
         this.listeEquipement = listeEquipement;
     }
     
-    private float coutGamme() {}
+//Methodes:
+    //suppression d'une gamme:
+      //voir directement dans Atelier pour supprimer une gamme de l'ensemble des gammes proposées.
     
+    //modifierGamme:
+    public void modifierGamme(ArrayList<Gamme> g, int i){
+        listGamme.set(i,g); //remplacer la gamme à la position i par la game g
+    }
     
-    
+    //afficher une gamme
+    public void AfficherGamme(Gamme gamme){
+    System.out.println("Pour réaliser la gamme de fabrication "+refGamme+" il faut les operations suivantes : "+listeOperations+" ainsi que les equipements suivants : "+listeEquipement);
 }
+        
+}
+//creerGamme () ;
+
+
+
+//Les méthodes de calcul suivantes :
+
+//coutGamme(), (retourne le coût d’une gamme donnée (float)
+
+//dureeGamme (), (calcule la durée d’exécution d’une gamme (float).
