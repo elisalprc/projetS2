34c0c0710702d597639b079fd5b6d0c7ea79f3c1
elisa <elisa@192.168.1.16>
2025-04-08 13:55:54 +0200

    premier test avec ajout toString

--- src/main/java/fr/insa/leprince/projets2/ProjetS2.java
+++ src/main/java/fr/insa/leprince/projets2/ProjetS2.java
@@ -8,6 +8,8 @@
 
 package fr.insa.leprince.projets2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author eleprince01
@@ -16,5 +18,19 @@ public class ProjetS2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ArrayList<Machine> LM = new ArrayList<>();
+        ArrayList<Poste> LPO = new ArrayList<>();
+        ArrayList<Produit> LPRO = new ArrayList<>();
+        ArrayList<Gamme> LG = new ArrayList<>();
+        ArrayList<Operateur> LOP = new ArrayList<>();
+        Atelier A1 = new Atelier("atelier test",LM,LPO,LPRO,LG,LOP);
+        
+        // on va entrer des infos dans l'atelier 
+        
+        Produit a = new Produit("a1","crayon");
+        A1.ajoutProduit(a);
+        System.out.println(A1.getListeProduits());  //!\\ il faut définir un toString pour chaque classe pour que ça s'affiche correctement 
+                                                    // ou autre chose mais je sais pas 
+        
     }
 }
