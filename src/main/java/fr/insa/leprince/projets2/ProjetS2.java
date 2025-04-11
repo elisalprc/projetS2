6b36c7cd81c31d1af5fb0a9c8b3cbb1b9b1d9f08
chach <chach@172.17.33.38>
2025-04-11 18:22:36 +0200

    Rebonsoir, voici les dernières nouvelles d'une petite tubercule !!
    Je viens de finir avec la classe FileReader et j'ai eu du mal mais j'ai l'impression que ça marche ! en tout cas j'ai pu récupérer les données mise dans le fichier texte en question (les lire) et en écrire de nouvelles dedans :) voilà on pourra saugegarder nos données et faire des stats ! 
    Par contre j'ai réfléchie et je pense qu'on aura un sacré problème ^^' parce que le fichier est chez moi sur mon ordi je veux dire... donc peut-être qu'il faut que vous en ouvriez un avec le même nom au même endroit ? et je ne suis pas sûr que si l'une d'entre nous écrit dedans les autres y aient accès alors peut être ouvrir un document partagé ? maix à voir si ça marche aussi avec les word et les fichiers goggle doc... Parce que là j'ai seulement utilisé un fichier text du bloc note ^^'
    
    voili voilou on en reparlera :) bisous bisous et bonne vacances encore !!

--- src/main/java/fr/insa/leprince/projets2/ProjetS2.java
+++ src/main/java/fr/insa/leprince/projets2/ProjetS2.java
@@ -8,6 +8,7 @@
 
 package fr.insa.leprince.projets2;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -16,8 +17,7 @@ import java.util.ArrayList;
  */
 public class ProjetS2 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
         // objets utiles pour creer les operateurs 
         ArrayList<Equipement> LEQ = new ArrayList<>();
         // pour l'atelier 
@@ -54,10 +54,51 @@ public class ProjetS2 {
         A1.ajoutPoste(poste1);
         System.out.println(A1.getListePostes());
         System.out.println(poste1.getListeMachine());
+  
         
+// =====================================================================================       
+// IMPORTANT : pour ecrire et lire dans un fichier texte donc sauvegarder des données !
+// =====================================================================================   
+        String file_name = "C:/Documents/Test"; //on aura besoin du nom du fichier par la suite ça risque de bloquer/buguer à ce niveau là :') vu que j'ai mis le fichier chez moi... ou alors on créée tous un fichier qu'on met au même endroit ???? en tout cas le path c'est le chemin pour y acceder :)
+        // pour lire un fichier texte
+        try{
+            ReadFile file = new ReadFile(file_name);
+            String[] lignes = file.OpenFile();
+            int i;
+            for (i=0; i<lignes.length;i++){
+                System.out.println(lignes[i]);
+            }
+        }
+        catch(IOException e){
+            System.out.println("Une erreur est survenue à la lecture, désolé, veuillez recommencer");           
+        }
         
-        
-        
-        
+        // pour écrire dans le fichier texte
+        try{
+            WriteFile patate  = new WriteFile(file_name, true); 
+            patate.writeToFile("Salut bonsoir je suis une patate ambulante ! "
+                    + "Quoi tu n'as jamais entendu parler de tubercule narrateur ? "
+                    + "Fichtre ! Je ne sais que dire sinon que je suis interloqué ! "
+                    + "Ma fierté en prends un sacré coup, il faut dire après tout que nous faisons parti avec les légumineuse des meilleurs conteurs et orateurs interstellaires ! "
+                    + "Dans bien de nombreux endroit où j'ai fait escale l'on m'acclamait à vive voix. "
+                    + "Tous s'époumonaient à hurler et quémander d'autres histoires. "
+                    + "Il faut dire que certains vivent tellement reclus que chaque histoire devient légende et à le gout d'or. "
+                    + "Quoi ? tu ne vois pas ce que c'est ??? Diantre mais qui est tu donc pour ignorer ceci ? "
+                    + "N'importe quel ignare même les plus grands qu'il m'ai été donné de croiser avaient tous entendu parlés de cette pierre fabuleuse ! "
+                    + "Roger c'est ça ? Moi c'est... enfin cela est sans importance après tout qu'est un nom sinon qu'une suite de lettre destinée à s'identifier ? "
+                    + "Je peux au moins te confier être une suivante de la Dame. Que dis tu ? Tu veux ma mort c'est bien ça ? "
+                    + "Ne pas connaitre la Dame c'est comme penser que nous sommes les seuls à respirer et festoyer ! "
+                    + "Pardi l'ami, c'est bien trop pour moi ! La Dame est en chaque coeur et chaque être ! "
+                    + "Elle fait foi dans le cosmère entier, foi de patate ! Alors mon petit Roger, si tu as un peu de temps, que dis-je, que tu en ai à disposition ou pas, assied toi auprès de moi et écoute ce que j'ai à te dire. "
+                    + "Ne pas connaitre l'or ou n'avoir jamais vu tubercule parler peut aller mais ignorer jusqu'à l'existence de la Dame, c'est dépasser les bornes... "
+                    + "Il y a fort longtemps mon brave si longtemps que rare sont les peuples y ayant assistés... "
+                    + "Non Roger, ce n'est pas une question de siècle ni de millénaires met toi bien ça dans le crâne, c'est tellement plus que même le plus sage de tes compatriotes ne saurait imaginer un temps aussi lointain. "
+                    + "Bien avant que le cosmère ne prenne entièrement forme, le rire empreint de légèreté et d'innocence d'une enfant éclatait, faisant de fait vibrer jusqu'aux entrailles de l'étoile sur laquelle elle et les siens se trouvaient. "
+                    + "L'étoile Irma. La jeune fille s'appelait Aeryn. Ahhh oui c'est bien elle, Aeryn est la Dame mais cesse donc de m'interrompre et écoute la suite...");
+                System.out.println("Text File Written To"); //pour dire que ça a bien écrit dans le fichier texte :)
+        }
+        catch(IOException e){
+            System.out.println("Une erreur est survenue à l'écriture, désolé, veuillez recommencer");           
+        }
     }
 }
